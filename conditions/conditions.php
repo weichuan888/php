3d82be4a92c1c5b2ecbab2d0174b12e7d6abc0d6
weichuan.tran <weichuan.tran@gmail.com>
2017-10-30 23:51:31 +0100

    test fin

---
+++ conditions/conditions.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Php Conditions</title>
+  </head>
+  <body>
+
+
+    $chambre_est_sale= true;
+    if ($chambre_est_sale=="true") {
+      $evenement = "Range ta chambre, on dirait la cage d'un bonobo!";
+    } else {
+      $evenement = "Ta chambre est trop propre, vis un peu!"
+    }
+
+
+
+
+
+
+
+
+
+  </body>
+</html>
--- variable/humans.php
+++ variable/humans.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +7,20 @@
     <title>php variable</title>
   </head>
   <body>
-      <h1>Bonjour <?php echo $_GET['Tran']; ?>!</h1>
-
-<!-- Initie une variable décrivant ton prénom.
-Ensuite, sur une seconde ligne, initie une variable qui représente ton âge.
-Ensuite, à la ligne suivante, initie une variable qui décrive si tu as faim ou pas.
-Ensuite, à la ligne suivante, initie une variable qui décrive la couleur de tes yeux.
-Ensuite, à la ligne suivante, initie une variable qui décrive ta famille (avec une variable Tableau, dont chaque entrée est le prénom d'un membre de ta famille). -->
-       $prenom= "weichuan";
-       $age= 24;
-       $hungry= true;
-       $couleur_yeux= "black";
-       $famille= array("Quang", "Tien", "Mei");
-      <?php echo "yo" ?>
+    <h1>Bonjour
+      <?php
+        echo $_GET['Tran'];
+      ?>
+    </h1>
+    <p>
+      <?php
+         $prenom= "weichuan";
+         $age= 24 ;
+         $hungry= true;
+         $couleur_yeux= "black";
+         $famille= array("Quang", "Tien", "Mei");
+         echo ("je m'appelle " . $prenom . " et j'ai " . $age . " ans ")
+      ?>
+    </p>
   </body>
 </html>
