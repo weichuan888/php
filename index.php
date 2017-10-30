3a371f022e375958b71693e5bb2042e6c8f25e69
weichuan.tran <weichuan.tran@gmail.com>
2017-10-30 14:03:37 +0100

    cre

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
+
 <?php
-echo "hello la terre !" ?>
+echo "hello a terre !";
+'<img url= "/var/www/html/php/php/chat.jpg">';
+ ?>
+ <!-- $nb = 56;
+ <p> Timon a <?="nb"?> ans </p> -->
+ <?php
+ echo "La police \"Arial\" est très utilisée ";
+ ?>
+<?php echo date('d/m/Y h:i:s'); ?>
