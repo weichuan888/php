7aae52506c815e2cf83f570c796cc3823472c653
weichuan.tran <weichuan.tran@gmail.com>
2017-10-30 14:21:15 +0100

    create file

---
+++ variable/humans.php
@@ -0,0 +1,13 @@
+<html>
+  <head><title>Hi!</title></head>
+  <body>
+    <h1>Bonjour <?php echo $_GET['Tran']; ?>!</h1>
+    <?php
+    $prenom= "weichuan";
+    $age= 24;
+    $faim= true;
+    $couleur_yeux= "black";
+    ?>
+    <?php echo "yo" ?>
+  </body>
+</html>
