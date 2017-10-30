defebbfee03af505c730ed6de98d69ca73ea667b
weichuan.tran <weichuan.tran@gmail.com>
2017-10-30 23:06:28 +0100

    test

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-echo "hello a terre !";
-'<img url= "/var/www/html/php/php/chat.jpg">';
- ?>
- <!-- $nb = 56;
- <p> Timon a <?="nb"?> ans </p> -->
- <?php
- echo "La police \"Arial\" est très utilisée ";
- ?>
-<?php echo date('d/m/Y h:i:s'); ?>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>test php</title>
+  </head>
+
+  <body>
+
+
+    <?php
+      echo "hello a terre !";
+
+     ?>
+     <!-- $nb = 56;
+     <p> Timon a <?="nb"?> ans </p> -->
+     <?php
+      echo "La police \"Arial\" est très utilisée ";
+     ?>
+     <?php
+      echo date('d/m/Y h:i:s');
+      ?>
+
+      <a href="index.php"> test </a>
+      <a href="cats.php"> cats </a>
+  </body>
+</html>
--- variable/humans.php
+++ variable/humans.php
@@ -2,13 +2,13 @@
   <head><title>Hi!</title></head>
   <body>
     <h1>Bonjour <?php echo $_GET['Tran']; ?>!</h1>
-    <?php
+    <?php echo
     $prenom= "weichuan";
     $age= 24;
     $faim= true;
     $couleur_yeux= "black";
     ?>
     <?php echo "yo" ?>
-		h1	
+		
   </body>
 </html>
