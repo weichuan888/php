6cb8067a7f9517a2dbbceee54f7d17fb500c98df
weichuan.tran <weichuan.tran@gmail.com>
2017-11-05 18:39:21 +0100

    exo4 fini

--- conditions/exo4.php
+++ conditions/exo4.php
@@ -5,65 +5,100 @@ Complète le formulaire de l'exercice précédent en ajoutant une troisième que
     Si l'âge est inférieur à 12 ans et que l'utilisateur répond "yes", retourne: "Hello boy!" ou "Hello Girl!" selon son genre.
     Si l'âge est entre 12 et 18 ans et que l'utilisateur répond "yes", affiche "Hello Teenage boy!" ou "Hello Teenage girl!" selon son genre.
     Si l'âge est entre 18 et 115 ans et que l'utilisateur répond "yes", affiche "Hello Sir!" ou "Hello Lady!" selon son genre. - Si l'âge est supérieur à 115 ans et que l'utilisateur répond "yes", affiche "Wow! Still alive, old man?" ou "old lady?" selon son genre. -->
-    <p>
+
          <form action="exo4.php" method="post">
-           <p>Quel est votre âge ?<input type="text" name="age" /></p>
-           <p>Genre:</p>
+           <label for="age"> Quel est votre age ?</label>
+           <input type="text" name="age" /></p>
+           <br/>
+           <label for="genre"> Genre :</label>
            <input name="genre" type="radio" value="homme">Homme
            <input name="genre" type ="radio" value="femme">Femme
-           <p>Parles-tu anglais ?</p>
-           <input type="radio" name="reponse" value="oui">Yes
-           <input type="radio" name="reponse" value="non">No
-           <p><input name ="envoi" type="submit" value="ok"></p>
+           <br/>
+           <br/>
+           <label for="anglais"> Parles-tu anglais ?</label>
+           <input type="radio" name="reponse" value="yes">Yes
+           <input type="radio" name="reponse" value="no">No
+           <br/>
+           <div class="button"><button type="submit" name="ok">ok</button>
+           </div>
          </form>
+
          <?php
-         $age=$_POST["age"];
-         $genre=$_POST["genre"];
-         $reponse=$_POST["reponse"]
+         extract($_POST);
 
          if (isset($_POST["envoi"])) {
            echo $_POST["genre"];
-           echo $_POST ["reponse"];
+           echo $_POST["reponse"];
          }
 
 
          if ($age <12) {
-           if($genre=='homme'){
-             if ($reponse=="oui") {
-                  $saluer= "Hello Boy!"
+           if ($genre=='homme'){
+             if ($reponse=="yes") {
+                $saluer= "Hello Boy!";
              }else {
-                  $saluer= "Salut petit!";
-             }
-             if ($reponse=="non") {
-                  $saluer= "Hello Girl!"
-             } else
-                  $saluer= "Salut petite!";
+                $saluer= "Salut petit!";
              }
            }
+           else if ($genre=='femme') {
+             if ($reponse=="no") {
+                $saluer= "Salut petite!";
+             } else {
+                $saluer= "Hello Girl!";
+             }
+           }
+         }
 
-      //  if ($age>=12 && $age <18) {
-      //      if($genre=='homme'){
-      //      $saluer= "Salut l'ado!";
-      //      }
-      //      else {
-      //        $saluer= "salut l'adolescente!";
-      //    }
-      //  }
-      //  if ($age>=18 && $age<115 && $genre =="homme") {
-      //        $saluer= "Salut l'adulte!";
-      //        }
-      //        else {
-      //          $saluer= "Salut la vieille";
-      //    }
-      //  if ($age>=115) {
-      //      if ($genre=='homme'){
-      //        $saluer= "Wow! Toujours vivant?";
-      //        }
-      //        else {
-      //          $saluer = "Wow! A quand ta mort?";
-      //        }
-      //    }
+        if ($age>=12 && $age <18) {
+           if ($genre=='homme'){
+             if($reponse=="yes") {
+                $saluer= "Hello Teenage Boy!";
+             } else {
+                $saluer= "Salut l'ado!";
+             }
+           }
+           else if ($genre=="femme") {
+             if ($reponse=="no") {
+               $saluer= "Salut l'adolescente!";
+             } else {
+               $saluer= "Hello Teenage Girl!";
+             }
+           }
+        }
+
+       if ($age>=18 && $age<115) {
+         if ($genre =="homme") {
+           if ($reponse=="yes") {
+              $saluer = "Hello Sir!";
+            } else {
+              $saluer= "Salut l'adulte!";
+            }
+          }
+          else if ($genre =="femme") {
+            if ($reponse=="no") {
+              $saluer = "Salut la vieille!";
+            } else {
+               $saluer= "Hello Lady";
+            }
+          }
+        }
+
+       if ($age>=115) {
+           if ($genre=='homme') {
+             if ($reponse=="yes") {
+               $saluer="Wow, still alive, old man !";
+             } else {
+              $saluer= "Wow! Toujours vivant?";
+             }
+           }
+           else if ($genre=="femme") {
+             if ($reponse== "no"){
+               $saluer ="Wow, a quand ta mort ?";
+             } else {
+               $saluer = "Wow, when are you going to died ?";
+             }
+           }
+        }
          echo $saluer;
 
          ?>
-     </p>
