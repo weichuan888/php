467b5e048159103583d03417873c9b06af4129f6
weichuan.tran <weichuan.tran@gmail.com>
2017-11-06 09:53:45 +0100

    tableau multi dimensionnels fini

--- tableaux/tableau1.php
+++ tableaux/tableau1.php
@@ -19,26 +19,73 @@
     <?php
     $pays = array( 'Belgique', 'France' , 'Allemagne', 'Pays-Bas', 'Ukraine' );
      echo $pays[2];
-
+     echo '<br/>';
 
     $famille = ["Père", "Mère", "Soeur"];
     print_r ($famille);
+    echo '<br/>';
 
     $plats= ["raclette", "steak", "pho", "poulet"];
     print_r ($plats);
+    echo '<br/>';
 
     $films_series= ["Big Bang Theory", "How I met your mother", "Friends", "Titanic"];
     echo $films_series [0];
-
-
+    echo '<br/>';
 
      ?>
+     <!-- Exercice : tableaux associatif
 
+    Décris-toi dans une tableau associatif: $moi.
+    Utilise au moins deux valeurs de chaque type:
+    texte (par exemple prenom),
+    booléenne (par exemple aime_le_foot) et
+    Int (par exemple: age, renvoie un chiffre). -->
 
+    <?php
+    $moi = [
+      'prenom' => 'Weichuan',
+      'age' => 24,
+      'aime_la_nourriture' => 'true',
+    ];
+    print_r ($moi);
+    echo '<br/>';
+    echo $moi ['aime_la_nourriture'];
+    echo '<br/>';
+    echo '<br/>';
+     ?>
+     <!-- Exercices : tableau multi-dimensionnels
 
+    Rajoute tes hobbies sous forme de tableau dans ton tableau $moi
+    Crée un deuxième tableau similaire au tien, qui décrive ton papa
+    (ou une personne de ta famille): $papa.
+    Assure-toi d'y avoir également décrit ses hobbies
+    Ensuite, à ton tableau $moi,
+    ajoute un élément dont la clef est 'papa' et la valeur équivaut à $papa.
+    Affiche le contenu de $moi via la fonction print_r().
+    (Utilise la balises html <pre> pour rendre cela plus lisible).
+    Relis ton code pour t'assurer de comprendre ce qui se passe à chaque ligne. -->
 
-
-
-
+    <?php
+    $papa = [
+      'prenom' => 'Quang',
+      'age' => 70,
+      'aime_la_nourriture' => true,
+      'hobbies' => array("IT", "lire", "photos")
+    ];
+    $moi = [
+      'prenom' => 'Weichuan',
+      'age' => 24,
+      'aime_la_nourriture' => 'true',
+      'hobbies' => array("musique", "jeux de société", "gastronomie"),
+      'papa' => $papa,
+    ];
+    echo '<pre>';
+    print_r ($moi);
+    echo '<pre>';
+    echo $moi['hobbies'][1];
+    echo '<br/>';
+    echo $moi ['papa']['hobbies'][1];
+     ?>
   </body>
 </html>
